b627c1aab45b3578a985c36d38a681ad7a4f1d7a
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-04-11 16:24:51 +0900

    ログインとログアウトのリンクを追加

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,24 @@ const layout = require("../layout");
 const app = new Hono();
 
 app.get("/", (c) => {
+  const { user } = c.get("session");
+
   return c.html(
     layout(
       "Home",
       html`
         <h1>Hello, Hono!</h1>
+        ${user
+          ? html`
+              <div>
+                <a href="/logout">${user.login} をログアウト</a>
+              </div>
+            `
+          : html`
+              <div>
+                <a href="/login">ログイン</a>
+              </div>
+            `}
       `,
     ),
   );
