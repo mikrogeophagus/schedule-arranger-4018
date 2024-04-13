8435571436083fc478ac7d08bd65ad33af8cc547
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-04-13 19:48:24 +0900

    予定の取得まで

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 const { Hono } = require("hono");
 const { html } = require("hono/html");
 const layout = require("../layout");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({ log: ["query"] });
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
   const { user } = c.get("session");
+  const schedules = user
+    ? await prisma.schedule.findMany({
+        where: { createdBy: user.id },
+        orderBy: { updatedAt: "desc" },
+      })
+    : [];
 
   return c.html(
     layout(
